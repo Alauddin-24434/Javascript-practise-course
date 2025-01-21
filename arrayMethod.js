239c62909427cb660ec525d0044f813b6b833c6c
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-01-22 00:49:03 +0600

    added loops

--- arrayMethod.js
+++ arrayMethod.js
@@ -10,13 +10,13 @@ console.log(city.length);
 
 // The concat() method returns a new array by merging two or more values/arrays.
 
-let primeNumbers = [2, 3, 5, 7]
-let evenNumbers = [2, 4, 6, 8]
+// let primeNumbers = [2, 3, 5, 7]
+// let evenNumbers = [2, 4, 6, 8]
 
 
-const joinedArrayes=primeNumbers.concat(evenNumbers);
+// const joinedArrayes=primeNumbers.concat(evenNumbers);
 
-console.log(joinedArrayes)
+// console.log(joinedArrayes)
 
 // 3. Javascript Array constructor
 // The constructor property returns the constructor function for the array.
@@ -62,6 +62,44 @@ console.log(msgRes)
 
 // 7. JavaScript Array reverse()
 // The reverse() method returns the array in reverse order.
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.reverse());
+
+//8.  Javascript Array reduce()
+// The reduce() method executes a reducer function on each element of the array and returns a single output value.
+// function joinString(accumuletor, currentValue){
+
+//     return accumuletor + currentValue;
+
+
+// }
+
+// const joinedString= message.reduce(joinString);
+
+// console.log(joinedString)
+// let numbers = [1, 2, 3, 4, 5];
+
+// function evenNumbers(arr){
+//   return arr.reduce ((acc, value)=>{
+//         if(value %2 ===0){
+//             return acc+ value
+//         }
+//         return acc;
+//     },0)
+// }
+
+// console.log(evenNumbers(numbers))
 let numbers = [1, 2, 3, 4, 5];
 
-console.log(numbers.reverse());
+
+function evenNumbersArry(arr){
+    return arr.reduce((evenArray, value)=>{
+        if(value % 2===0){
+          evenArray.push(value) 
+        }
+        return evenArray;
+    },[])
+}
+
+console.log(evenNumbersArry(numbers))
---
+++ loops.js
@@ -0,0 +1,10 @@
+
+let initalValue;
+
+for (initalValue=0 ; initalValue < 4 ; initalValue ++){
+  console.log("inner", initalValue)
+}
+
+console.log("out", initalValue)
+
+
