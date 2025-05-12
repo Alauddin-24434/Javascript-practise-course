1687750e70136a51f93b4a0a1058744cb8c370ae
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-05-12 23:18:57 +0600

    practuse

--- Practise.js
+++ Practise.js
@@ -1,7 +1,29 @@
-// sum two num  of two numbers
+// sum   of two numbers
 
 function sumOfTwoNumbers(a,b){
   return a+b;
 }
 
 console.log(sumOfTwoNumbers(2,4))
+
+
+// multple of two numbers
+
+function multpleTwoNumbers(a,b){
+  return a*b;
+};
+
+console.log(multpleTwoNumbers(2,4));
+
+
+// devided of twonumbers
+
+function devidedTwoNumbers(a,b){
+return a/b;
+}
+
+console.log(devidedTwoNumbers(6,5))
+
+
+// higher order function
+
